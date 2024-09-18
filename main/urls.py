dca7be266c98ad5b8cd374349e0640093d70b831
DaoistXuandu <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>
2024-09-18 15:06:21 +0700

    add logauth

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
-from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
 
 app_name = 'main'
 
 urlpatterns = [
+    path('login/', login_user, name='login'),
     path('', show_main, name='show_main'),
     path('create-mood-entry', create_mood_entry, name='create_mood_entry'),
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
     path('json/', show_json, name='show_json'),
-    path('xml/', show_xml, name='show_xml')
+    path('xml/', show_xml, name='show_xml'),
+    path('login/', login_user, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', logout_user, name='logout'),
 ]
